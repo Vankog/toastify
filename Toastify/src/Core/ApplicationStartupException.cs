86db5e1a060a781a29deb636319e01533bfe7ab3
Vankog <m.vankog@gmail.com>
2018-04-03 16:39:38 +0200

    Gets rid of unnecessary constructors and cleans up message creation.

--- Toastify/src/Core/ApplicationStartupException.cs
+++ Toastify/src/Core/ApplicationStartupException.cs
@@ -5,44 +5,34 @@ namespace Toastify.Core
 {
     public class ApplicationStartupException : ApplicationException
     {
-        public ApplicationStartupException() : this(true)
+        public ApplicationStartupException(string message) : base(message + Environment.NewLine + CreateMessage())
         {
         }
 
-        public ApplicationStartupException(bool spotifyStatus) : base(CreateMessage(spotifyStatus))
+        private static string CreateMessage()
         {
-        }
+            var nl = Environment.NewLine;
+            if (Spotify.Instance == null)
+                return string.Join(nl, "No Spotify instance created.");
 
-        public ApplicationStartupException(string message) : this(message, true)
-        {
-        }
-
-        public ApplicationStartupException(string message, bool spotifyStatus) : base($"{CreateMessage(spotifyStatus)}\nAdditional details:\n{message}")
-        {
-        }
-
-        private static string CreateMessage(bool spotifyStatus = true)
-        {
             List<string> messages = new List<string>();
+            messages.Add("Spotify is running: " + Spotify.Instance.IsRunning);
 
-            if (spotifyStatus)
+            var status = Spotify.Instance.Status;
+            messages.Add("Status: ");
+            if (status == null)
+                messages.Add("null");
+            else
             {
-                bool spotifyInstanceCreated = Spotify.Instance != null;
-                if (spotifyInstanceCreated)
-                {
-                    bool spotifyIsRunning = Spotify.Instance.IsRunning;
-                    messages.Add($"Spotify is running: {spotifyIsRunning}");
-
-                    var status = Spotify.Instance.Status;
-                    messages.Add(status != null
-                        ? $"Status: {status.Online}, {status.Running}, {status.Version}, {status.ClientVersion}, {status.Track != null}"
-                        : "Status: null");
-                }
-                else
-                    messages.Add("Spotify instance not created!");
+                // TODO: add a status.toString() to Spotify API and replace this:
+                messages.Add(nl + "-- Online: " + status.Online);
+                messages.Add(nl + "-- Running: " + status.Running);
+                messages.Add(nl + "-- Version: " + status.Version);
+                messages.Add(nl + "-- Client Version: " + status.ClientVersion);
+                messages.Add(nl + "--  has Track: " + (status.Track != null));
             }
 
-            return string.Join(Environment.NewLine, messages);
+            return string.Join(nl, messages);
         }
     }
 }
