9d76df6aefa72f88e3cbe58fa9fd3304e12bfb43
Vankog <m.vankog@gmail.com>
2018-04-04 18:56:51 +0200

    formatting adjustments to ApplicationStartupException Aleab/Toastify#64

--- Toastify/src/Core/ApplicationStartupException.cs
+++ Toastify/src/Core/ApplicationStartupException.cs
@@ -11,31 +11,41 @@ namespace Toastify.Core
 
         private static string CreateMessage()
         {
-            // Touching Spotify.Instance forces instance creation, but it fails if called too early. Therefore, a try is needed.
+            // Touching Spotify.Instance forces instance creation if none, but it fails if called too early. 
+            // Therefore, a try is needed.
             try
             {
-                var tmp = Spotify.Instance;
+                // unused variable is needed to build the project
+                var burner = Spotify.Instance;
             }
             catch (NullReferenceException)
             {
-                return "Spotify instance status: not yet created.";
+                return "\tSpotify instance status: not yet created.";
             }
 
             List<string> messages = new List<string>();
-            messages.Add("Spotify instance running status: " + Spotify.Instance.IsRunning);
+            messages.Add($"\tSpotify instance running status: {Spotify.Instance.IsRunning}");
 
             var status = Spotify.Instance.Status;
-            messages.Add("Spotify instance Status: ");
             if (status == null)
-                messages.Add("null");
+                messages.Add("\tSpotify instance status: null");
             else
             {
-                // TODO: add a status.toString() to Spotify API and replace this:
-                messages.Add("-- Online: " + status.Online);
-                messages.Add("-- Running: " + status.Running);
-                messages.Add("-- Version: " + status.Version);
-                messages.Add("-- Client Version: " + status.ClientVersion);
-                messages.Add("--  has Track: " + (status.Track != null));
+                messages.Add("\tSpotify instance status: ");
+                messages.Add($"\t-- Version:\t{status.Version}");
+                messages.Add($"\t-- Client Version:\t{status.ClientVersion}");
+                messages.Add($"\t-- Playing:\t{status.Playing}");
+                messages.Add($"\t-- Shuffle:\t{status.Shuffle}");
+                messages.Add($"\t-- Repeat:\t{status.Repeat}");
+                messages.Add($"\t-- Play Enabled:\t{status.PlayEnabled}");
+                messages.Add($"\t-- Prev Enabled:\t{status.PrevEnabled}");
+                messages.Add($"\t-- Next Enabled:\t{status.NextEnabled}");
+                messages.Add($"\t-- has Track:\t{status.Track != null}");
+                messages.Add($"\t-- Server Time:\t{status.ServerTime}");
+                messages.Add($"\t-- Volume:\t{status.Volume}");
+                messages.Add($"\t-- Online:\t{status.Online}");
+                messages.Add($"\t-- has OpenGraphState: {status.OpenGraphState != null}");
+                messages.Add($"\t-- Running:\t{status.Running}");
             }
 
             return string.Join(Environment.NewLine, messages);
